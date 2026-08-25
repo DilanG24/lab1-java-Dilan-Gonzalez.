cf402219bb500aa0b258b41004686d3794d55608
DilanG24 <dilan.gonzalez1@utp.ac.pa>
2026-08-25 15:05:01 -0500

    Instantiate and display new Auto object

--- Laboratorio1.java
+++ Laboratorio1.java
@@ -72,10 +72,11 @@ public class Laboratorio1 {
         // TODO 2: Asigna valores a sus atributos (marca, modelo y precioAlquiler).
         // TODO 3: Llama al método mostrarDatos() para el objeto 'auto2'.
     }
-    // Objeto de prueba
+   
+}
+ // Objeto de prueba
         Auto auto2 = new Auto();
         auto2.marca = "Nissan";
         auto2.modelo = "Versa";
         auto2.precioAlquiler = 28.0;
         auto2.mostrarDatos();
-}
